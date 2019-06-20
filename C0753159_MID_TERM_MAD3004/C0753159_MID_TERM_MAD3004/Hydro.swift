7afd7d3a02651129ced804c3705851f072cf5659
Nikhil Nikhil <nikhilthakur050@gmail.com>
2019-06-20 19:48:05 -0400

    hydro

---
+++ C0753159_MID_TERM_MAD3004/C0753159_MID_TERM_MAD3004/Hydro.swift
@@ -0,0 +1,27 @@
+//
+//  Hydro.swift
+//  C0753159_MID_TERM_MAD3004
+//
+//  Created by MacStudent on 2019-06-20.
+//  Copyright © 2019 Nikhil nikhil. All rights reserved.
+//
+
+import Foundation
+class Hydro : Bill
+{
+    var agencyName : String?
+    var consumedUnites: Float?
+    
+    
+    init( billId :Int, billDate : Date, billType : String, totalAmount : Float,agencyName: String, consumedUnites : Float)
+    {
+        self.agencyName = agencyName
+        self.consumedUnites = consumedUnites
+        super.init(billId:billId,billDate:billDate,billType:billType,totalAmount:totalAmount)
+    }
+    override func display()
+    {
+        print(self.billId!, self.billDate!.getFormatDate(),self.billType!,self.totalAmount!.dollar(), self.!, self.IntGB!.gb(), separator:"\t")
+    }
+    
+}
--- C0753159_MID_TERM_MAD3004/C0753159_MID_TERM_MAD3004/Mobile.swift
+++ C0753159_MID_TERM_MAD3004/C0753159_MID_TERM_MAD3004/Mobile.swift
@@ -7,4 +7,27 @@
 //
 
 import Foundation
-
+class Mobile : Bill
+{
+    var manufacturername : String?
+    var planName : String?
+    var mobileno : Int?
+    var internetused : Int?
+    var muniteused : String?
+    
+    init( billId :Int, billDate : Date, billType : String, totalAmount : Float,manufacturername: String,  planeName : String, mobileno :Int, internetused : Int, muniteused : String)
+    {
+        self.manufacturername = manufacturername
+        self.planName = planeName
+        self.mobileno = mobileno
+        self.internetused = internetused
+        self.muniteused = muniteused
+        
+        super.init(billId:billId,billDate:billDate,billType:billType,totalAmount:totalAmount)
+    }
+    override func display()
+    {
+        print(self.billId!, self.billDate!.getFormatDate(),self.billType!,self.totalAmount!.dollar(), self.manufacturername!,self.planName!,self.mobileno!,self.internetused!.gb(), self.muniteused!, separator:"\t")
+    }
+    
+}
