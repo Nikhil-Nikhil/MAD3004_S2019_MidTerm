7f5f6fd0dc81257fd168dd6f3d7ca75a370ab102
Nikhil Nikhil <nikhilthakur050@gmail.com>
2019-06-20 21:09:10 -0400

    bill in main

--- C0753159_MID_TERM_MAD3004/C0753159_MID_TERM_MAD3004/main.swift
+++ C0753159_MID_TERM_MAD3004/C0753159_MID_TERM_MAD3004/main.swift
@@ -20,3 +20,12 @@ var bill1 = Bill(billId: 1,billDate: Date(), billType :"Hydro",totalAmount: 300)
 var bill2 = Bill(billId: 2,billDate: Date(), billType :"Internet",totalAmount: 300)
 var bill3 = Bill(billId: 3,billDate: Date(), billType :"Mobile",totalAmount: 300)
 c1.display()
+
+
+
+if let myBill = Bill.getbillById(billId: 2)
+{
+    print(myBill.display())
+}else{
+    print("NO Bill FOUND WITH GIVEN ID")
+}
